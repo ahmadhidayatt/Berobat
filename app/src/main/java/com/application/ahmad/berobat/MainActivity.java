e9df91a9b8a5bf959074758941cf49afa880706f
ahmadhidayatt <comp.ahmadhidayat@gmail.com>
2017-10-03 17:06:07 +0700

    init

--- app/src/main/java/com/application/ahmad/berobat/MainActivity.java
+++ app/src/main/java/com/application/ahmad/berobat/MainActivity.java
@@ -1,13 +1,42 @@
 package com.application.ahmad.berobat;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
+    private WebView view; //membuat variabel view agar bisa akses method
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        view = (WebView) this.findViewById(R.id.webView);
+        view.getSettings().setJavaScriptEnabled(true);
+        view.setWebViewClient(new MyBrowser());
+        view.loadUrl("http://berobat.id/#Login");
     }
+
+    private class MyBrowser extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+    }
+
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        //ketika disentuh tombol back
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && view.canGoBack()) {
+            view.goBack(); //method goback(),untuk kembali ke halaman sebelumnya
+            return true;
+        }
+        // Jika tidak ada halaman yang pernah dibuka
+        // maka akan keluar dari activity (tutup aplikasi)
+        return super.onKeyDown(keyCode, event);
+    }
+
 }
